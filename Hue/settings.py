9a8e5e3fd58684191f0b942108afab4e8661a252
Alissa Troiano <alissatroianonyc@gmail.com>
2021-04-15 23:31:04 +0000

    Remove whitespace from settings.py and repair html error in base template

--- Hue/settings.py
+++ Hue/settings.py
@@ -128,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -141,7 +140,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
