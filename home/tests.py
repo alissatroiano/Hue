6efc4544f4633bcdece3cbb0ad88f141cc85a65b
Alissa Troiano <alissatroianonyc@gmail.com>
2021-05-07 15:06:51 +0000

    Run some initial tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works2(self):
+        self.assertEqual(1, 0)
+
+    def test_this_thing_works3(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_work4(self):
+        self.assertEqual(1, 30)
