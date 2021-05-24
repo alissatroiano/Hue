898443f8a362a0ddbe5a2051920e9ddcd68e43a7
Alissa Troiano <alissatroianonyc@gmail.com>
2021-05-24 16:57:05 -0400

    Add functionality to search & test manually

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -7,9 +9,22 @@ def shop_all(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query= None
+
+    # https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/673a36fdd4bb2c09f8843c6ad8cb6ae4a60dda01/templates/includes/main-nav.html
+    if request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('shop'))
+    
+        queries = Q(title__icontains=query) | Q(product_details__icontains=query)
+        products = products.filter(queries, autocomplete_fields = ['title'])
+
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'shop/shop.html', context)
