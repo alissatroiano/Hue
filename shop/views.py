61e2ea8446fdf7f10fe7e68e4cf157481246a8ea
Alissa Troiano <alissatroianonyc@gmail.com>
2021-05-25 19:00:34 -0400

    Add sort functionality to shop and test in live server

--- shop/views.py
+++ shop/views.py
@@ -10,21 +10,47 @@ def shop_all(request):
 
     products = Product.objects.all()
     query= None
+    categories = None
+    sort = None
+    direction = None
 
     # https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/673a36fdd4bb2c09f8843c6ad8cb6ae4a60dda01/templates/includes/main-nav.html
     if request.GET:
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "You didn't enter any search criteria!")
-            return redirect(reverse('shop'))
-    
-        queries = Q(title__icontains=query) | Q(product_details__icontains=query)
-        products = products.filter(queries, autocomplete_fields = ['title'])
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
 
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('shop'))
+        
+            queries = Q(title__icontains=query) | Q(product_details__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'shop/shop.html', context)
