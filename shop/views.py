05be00e5d58f28361426d95534814b1826a87171
Alissa Troiano <alissatroianonyc@gmail.com>
2021-05-27 11:24:24 -0400

    Add category sorting functionality to views.py

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
@@ -31,8 +31,8 @@ def shop_all(request):
             
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            products = products.filter(category__title__in=categories)
+            categories = Category.objects.filter(title__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
