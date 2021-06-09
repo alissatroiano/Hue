8d0b7543a144c7e98092a6a431157cbc08cb9014
Alissa Troiano <alissatroianonyc@gmail.com>
2021-06-09 12:49:18 -0400

    Setup project to prepare for Heroku deployment

--- Hue/settings.py
+++ Hue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
