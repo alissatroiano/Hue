4df4f247485c69da30778b5980a3baa58188400b
Alissa Troiano <alissatroianonyc@gmail.com>
2021-06-10 13:48:36 -0400

    Remove trailing slash to repair Heroku deployment

--- Hue/settings.py
+++ Hue/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hue-alissa.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['hue-alissa.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
