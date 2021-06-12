35c3701bd2f9f864a541b52bab828de803bfe8da
Alissa Troiano <alissatroianonyc@gmail.com>
2021-06-12 19:57:45 -0400

    Create add to cart functionality and test in live browser

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -6,3 +6,20 @@ from django.shortcuts import render
 def view_cart(request):
     """ A view to return the index page """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ A view to add a quantity of a specific store product to the user's shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
