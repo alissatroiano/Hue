6816411b9d9ba034e319612cfa166a1cbe0b4e0d
Alissa Troiano <alissatroianonyc@gmail.com>
2021-06-12 20:54:54 -0400

    Create table that renders order details in cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from shop.models import Product
 
 
 def cart_items_function(request):
@@ -7,6 +9,17 @@ def cart_items_function(request):
 	cart_items = []
 	total= 0
 	product_count = 0
+	cart = request.session.get('cart', {})
+
+	for item_id, quantity in cart.items():
+		product = get_object_or_404(Product, pk=item_id)
+		total += quantity * product.price
+		product_count += quantity
+		cart_items.append({
+			'item_id': item_id,
+			'quantity': quantity,
+			'product': product,
+		})
 
 	
 	if total < settings.PROMOTION_MINIMUM:
--- cart/views.py
+++ cart/views.py
@@ -21,5 +21,4 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
