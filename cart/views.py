2f2cff603f82c96f4ea9e5c8c5aa2d2828ca496d
Alissa Troiano <alissatroianonyc@gmail.com>
2021-07-07 14:51:34 -0400

    Remove whitespace

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 
-
-# Create your views here.
-
 def view_cart(request):
     """ A view to return the index page """
     return render(request, 'cart/cart.html')
