6b243fbc78a49c0c94b2228a51b106d2259ff1c7
Alissa Troiano <alissatroianonyc@gmail.com>
2021-07-13 12:38:52 -0400

    Add logic to connect allauth users to profile and render on front-end

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
 
 # Create your views here.
 def profile(request):
+    """ 
+    Display the user's profile 
+    """
+    profile = get_object_or_404(Profile, user=request.user)
     
-    return render(request, 'profiles/profile.html')
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
