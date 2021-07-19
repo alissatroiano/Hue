42892dec9ffe7c3839e9e39b9ab7a2af83faf6b5
Alissa Troiano <alissatroianonyc@gmail.com>
2021-07-19 13:39:55 -0400

    Create product admin product form

---
+++ shop/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Category, Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-1 rounded shadow-sm'
