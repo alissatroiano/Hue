807454956c12e8deb74112db6811d47c4bf1e996
Alissa Troiano <alissatroianonyc@gmail.com>
2021-07-23 14:04:57 -0400

    Repair typo in email config

--- Hue/settings.py
+++ Hue/settings.py
@@ -221,7 +221,7 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
