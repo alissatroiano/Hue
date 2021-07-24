d107cd6751d99fc61b3dc4235acbbd10dd10d855
Alissa Troiano <alissatroianonyc@gmail.com>
2021-07-24 14:13:05 -0400

    Update debug settings for production

--- Hue/settings.py
+++ Hue/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hue-alissa.herokuapp.com', 'localhost', '127.0.0.1']
 
