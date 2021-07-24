7e25ddd582b416ba36db8b978f095d5ac029b552
Alissa Troiano <alissatroianonyc@gmail.com>
2021-07-24 16:02:57 -0400

    Repair error in email config

--- Hue/settings.py
+++ Hue/settings.py
@@ -222,7 +222,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 if 'USE_AWS' in os.environ:
