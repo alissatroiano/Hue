f5b36a35fe3ca4f2b6beb5738e39836b52c5024b
Alissa Troiano <alissatroianonyc@gmail.com>
2021-07-28 11:25:03 -0400

    Add if/else statement so image URL does not require validation

--- shop/forms.py
+++ shop/forms.py
@@ -23,4 +23,7 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-1 rounded shadow-sm'
+        if field.label == 'Image URL':
+            field.widget.attrs['required'] = False
+        else:
             field.widget.attrs['required'] = True
