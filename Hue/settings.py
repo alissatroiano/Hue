56ce04ed3aa24ea6301a47281001834e4abf1958
Alissa Troiano <alissatroianonyc@gmail.com>
2021-08-09 14:37:23 -0400

    Change DB settings to avoid conflict with production build

--- Hue/settings.py
+++ Hue/settings.py
@@ -127,15 +127,10 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 WSGI_APPLICATION = 'Hue.wsgi.application'
 
 # Database
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-        }
-else:
-    DATABASES = {
-        'default': {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mydatabase',
     }
 }
 # Password validation
