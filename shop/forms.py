581075cf5378c5604f12501f35ac221558226611
alissatroiano <alissatroianonyc@gmail.com>
2021-11-15 12:23:47 -0500

    Update forms.py to repair bug in edit product form

--- shop/forms.py
+++ shop/forms.py
@@ -4,11 +4,11 @@ from .models import Category, Product
 
 
 class ProductForm(forms.ModelForm):
-    
+
     class Meta:
         model = Product
         fields = '__all__'
-        
+
         # image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
@@ -23,4 +23,9 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-1 rounded shadow-sm'
-            field.widget.attrs['required'] = True
+            if field_name == 'image_url':
+                field.widget.attrs['required'] = False
+            elif field_name == 'image':
+                field.widget.attrs['required'] = False
+            else:
+                field.widget.attrs['required'] = True
