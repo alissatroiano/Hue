e8e9a1001b6edcd79e38ffdfc69de6f64d9bdf28
Alissa <alissatroianonyc@gmail.com>
2023-05-24 23:06:49 -0400

    Add function for using prompty to generate image and test

--- hugo/views.py
+++ hugo/views.py
@@ -42,24 +42,24 @@ def add_hugo(request):
                 size="1024x1024"
             )
             image_url = response['data'][0]['url']
-            return image_url
+            print(image_url)
 
     return render(request, 'add_hugo.html', {'form': form})
 
 
-@login_required
-def get_title_suggestions(request):
-    form = ArtworkForm()
-    predicted_titles = []
-    if request.method == 'POST':
-        # Retrieve the artwork description from the POST request
-        text = request.POST.get('text')
+# @login_required
+# def get_title_suggestions(request):
+#     form = ArtworkForm()
+#     predicted_titles = []
+#     if request.method == 'POST':
+#         # Retrieve the artwork description from the POST request
+#         text = request.POST.get('text')
 
-        # Call your MindsDB/ChatGPT model to get the predictions
-        mdb_server = mindsdb_sdk.connect('https://cloud.mindsdb.com', settings.MINDSDB_EMAIL, settings.MINDSDB_PASSWORD)
-        project = mdb_server.get_project('open_ai')
-        query = project.query(f'SELECT * FROM open_ai.art WHERE artwork_description="{text}";')
+#         # Call your MindsDB/ChatGPT model to get the predictions
+#         mdb_server = mindsdb_sdk.connect('https://cloud.mindsdb.com', settings.MINDSDB_EMAIL, settings.MINDSDB_PASSWORD)
+#         project = mdb_server.get_project('open_ai')
+#         query = project.query(f'SELECT * FROM open_ai.art WHERE artwork_description="{text}";')
 
-        predicted_titles = DataFrame.to_dict(query.fetch(), orient='records')
+#         predicted_titles = DataFrame.to_dict(query.fetch(), orient='records')
     
-    return render(request, 'shop/get_title_suggestions.html', {'predicted_titles': predicted_titles})
+#     return render(request, 'shop/get_title_suggestions.html', {'predicted_titles': predicted_titles})
