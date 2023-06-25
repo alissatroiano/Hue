ec7117c7f6e965b19a7e98035d4be7926c3a152a
Alissa <alissatroianonyc@gmail.com>
2023-06-24 20:34:48 -0400

    Create new model

--- hugo/admin.py
+++ hugo/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.conf import settings
-from .models import userArtwork, Hugo
+from .models import Hugo, Artwork
+
 
 # Register your models here.
-
-@admin.register(userArtwork)
-class userArtworkAdmin(admin.ModelAdmin):
-    fields = ('artwork', 'titles', 'predicted_titles', 'user')
-
-
 @admin.register(Hugo)
 class HugoAdmin(admin.ModelAdmin):
-    fields = ('name', 'user', 'artwork_description', 'artwork', 'image_url', 'genArt', 'titles', 'predicted_titles')
+    fields = [
+        'name', 'user', 'artwork_description', 'img_url', 'image'
+    ]
+
+@admin.register(Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    fields = [  
+        'title', 'user', 'artwork_description', 'image_url', 'image'
+    ]
--- hugo/models.py
+++ hugo/models.py
@@ -2,31 +2,41 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-class userArtwork(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    artwork = models.ImageField(upload_to='artworks', blank=True, null=True)
-    titles = models.TextField(blank=True)
-    predicted_titles = models.JSONField(blank=True, null=True)
-
-    def __str__(self):
-        return self.artwork.name
-
-
 class Hugo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=254, default='')
-    artwork_description = models.TextField(blank=True)
-    artwork = models.ImageField(upload_to='artworks', blank=True, null=True)
-    genArt = models.ImageField(upload_to='artworks', default="banana.png")
-    titles = models.TextField(blank=True)
-    predicted_titles = models.JSONField(blank=True, null=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    name = models.CharField(max_length=254, unique=True)
+    artwork_description = models.TextField(null=True, blank=True)
+    img_url = models.URLField(max_length=1024, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
     class Meta:
-            verbose_name_plural = 'Hugos'
+            verbose_name_plural = 'hugos'
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+    
+
+class Artwork(models.Model):
+    id = models.AutoField(primary_key=True)
+    artwork_description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=254, unique=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, related_name='userz', on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'artworks'
+
+    def save(self, *args, **kwargs):
+             super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
