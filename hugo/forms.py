ed1bba1f9743983d7a7336e542c5b2046a1f2404
Alissa <alissatroianonyc@gmail.com>
2023-06-24 20:04:22 -0400

    Update views, forms for adding artwork

--- hugo/forms.py
+++ hugo/forms.py
@@ -4,4 +4,27 @@ from .models import Hugo
 class HugoForm(forms.ModelForm):
     class Meta:
         model = Hugo
-        fields = ['user', 'name']
+        fields = ['artwork_description', 'name']
+
+    def __init__(self, *args, **kwargs):
+            """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+            """
+            super().__init__(*args, **kwargs)
+
+            for field_name, field in self.fields.items():
+                """ Set a length requirement for the artwork description """
+                if field_name == 'artwork_description':
+                    field.widget.attrs['maxlength'] = 500
+                    field.widget.attrs['rows'] = 3
+                    field.widget.attrs['placeholder'] = 'Describe your artwork'
+                    field.widget.attrs['class'] = 'border-1 rounded shadow-sm'
+                    field.widget.attrs['required'] = True
+                if field_name == 'name':
+                    field.widget.attrs['placeholder'] = 'Give your artwork a name'
+                    field.widget.attrs['class'] = 'border-1 rounded shadow-sm'
+                    field.widget.attrs['required'] = True
+                else:
+                    field.widget.attrs['class'] = 'border-1 rounded shadow-sm'
+                    field.widget.attrs['required'] = True
--- hugo/views.py
+++ hugo/views.py
@@ -15,15 +15,14 @@ password = os.environ["MINDSDB_PASSWORD"]
 
 def hugo(request):
     """ A view to show all a user's hugos """
-    hugos = Hugo.objects.all()
+    hugo = Hugo.objects.all()
     image_url = None
 
     if request.GET:
-        hugos = hugos.order_by('-created_at')
+        hugo = hugo.order_by('-created_at')
         image_url = request.GET['image_url']
 
     context = {
-        'hugos': hugos,
         'image_url': image_url,
     }
 
@@ -32,32 +31,50 @@ def hugo(request):
 
 @login_required
 def add_hugo(request):
-    form = HugoForm()
-    hugo = Hugo()
-    image_url= []
+# A view to add a hugo to the database
     if request.method == 'POST':
-        # Retrieve the artwork description from the POST request
-        text = request.POST.get('artwork_description')
-
-        # Call your MindsDB/ChatGPT model to get the predictions
-        mdb_server = mindsdb_sdk.connect('https://cloud.mindsdb.com', settings.MINDSDB_EMAIL, settings.MINDSDB_PASSWORD)
-        project = mdb_server.get_project('open_ai')
-        query = project.query(f'SELECT * FROM open_ai.art_model WHERE text="{text}";')
-        print(query.fetch())
-        image_url = (query.fetch())
-        print(image_url)
-
         form = HugoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            hugo = Hugo.objects.create(
-                name=request.POST.get('name'),
-                user=request.user,
-                artwork_description=request.POST.get('artwork_description'),
-                image_url=query.fetch('img_url')'),
-            )
-            hugo.save()
-            messages.success(request, 'Successfully added Hugo!')
-            return redirect('add_hugo')
+            return redirect('hugo')
+    else:
+        form = HugoForm()
+
+    template = 'add_hugo.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+            
+
+# @login_required
+# def add_hugo(request):
+#     form = HugoForm()
+#     hugo = Hugo()
+#     image_url= []
+#     if request.method == 'POST':
+#         # Retrieve the artwork description from the POST request
+#         text = request.POST.get('artwork_description')
+
+#         # Call your MindsDB/ChatGPT model to get the predictions
+#         mdb_server = mindsdb_sdk.connect('https://cloud.mindsdb.com', settings.MINDSDB_EMAIL, settings.MINDSDB_PASSWORD)
+#         project = mdb_server.get_project('open_ai')
+#         query = project.query(f'SELECT * FROM open_ai.art_model WHERE text="{text}";')
+#         print(query.fetch())
+#         image_url = (query.fetch())
+#         print(image_url)
+
+#         form = HugoForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             hugo = Hugo.objects.create(
+#                 name=request.POST.get('name'),
+#                 user=request.user,
+#                 artwork_description=request.POST.get('artwork_description'),
+#                 image_url=query.fetch()),            
+#             hugo.save()
+#             messages.success(request, 'Successfully added Hugo!')
+#             return redirect('add_hugo')
     
-    return render(request, 'add_hugo.html', {'form': form, 'image_url': image_url})
+#     return render(request, 'add_hugo.html', {'form': form, 'image_url': image_url})
