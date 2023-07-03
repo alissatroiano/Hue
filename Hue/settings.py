bc7016724ca3e5b90de270f3bd3630b8674c4ddf
Alissa <alissatroianonyc@gmail.com>
2023-07-02 21:18:46 -0400

    Dump data

--- Hue/settings.py
+++ Hue/settings.py
@@ -145,12 +145,17 @@ if 'DATABASE_URL' in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+   DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hue_db',
+        'USER': 'alissa',
+        'PASSWORD': 'P!n3apple-%4200',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
