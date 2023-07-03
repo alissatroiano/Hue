eff68f23d6117c3f8802789180203366499bfe67
Alissa <alissatroianonyc@gmail.com>
2023-07-02 22:33:40 -0400

    remove static files config

--- Hue/settings.py
+++ Hue/settings.py
@@ -207,7 +207,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media
 MEDIA_URL = '/media/'
