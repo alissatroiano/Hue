3c15195810be8b0a89737f17c63bd48885ac08f9
Alissa <alissatroianonyc@gmail.com>
2023-08-10 16:27:17 -0400

    Clean up directory

--- artists/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ArtistInfo
-
-# Register your models here.
-
-
-@admin.register(ArtistInfo)
-class ArtistInfoAdmin(admin.ModelAdmin):
-    fields = ('artist', 'photo')
