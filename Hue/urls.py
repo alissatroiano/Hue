c65fc4e94723ef759e26707afaf7c64904a023ba
Alissa <alissatroianonyc@gmail.com>
2023-08-21 11:17:03 -0400

    Update url patterns defined in main urls.py

--- Hue/urls.py
+++ Hue/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     url(r'^accounts/', include('allauth.urls')),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('shop/', include('shop.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
