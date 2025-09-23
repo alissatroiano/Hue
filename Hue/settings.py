ed8f39fc9e276ec8fd36425d702523bf4586d31e
Alissa Troiano <alissatroianonyc@gmail.com>
2025-09-23 17:31:12 -0400

    Update project index page

--- Hue/settings.py
+++ Hue/settings.py
@@ -221,7 +221,7 @@ if USE_AWS:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = 'public-read' # Or 'private' depending on your needs
+    AWS_DEFAULT_ACL = 'public-read'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIAFILES_LOCATION = '/media/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
