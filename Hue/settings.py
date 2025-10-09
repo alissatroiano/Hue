bba383742683897d01897fcbbd3caf2d14dc9329
Alissa Troiano <alissatroianonyc@gmail.com>
2025-10-09 17:22:49 -0400

    Update settings with S3 config and custom storages

--- Hue/settings.py
+++ Hue/settings.py
@@ -133,6 +133,9 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
 }
+    
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
