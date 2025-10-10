6d3b98193a97ba5d093d830ead3c25b6883564e3
Alissa Troiano <alissatroianonyc@gmail.com>
2025-10-10 00:27:24 -0400

    Add static root

--- Hue/settings.py
+++ Hue/settings.py
@@ -167,6 +167,7 @@ TIME_ZONE = 'UTC'
 # Static files (CSS, JavaScript, Images)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
@@ -175,7 +176,6 @@ STATICFILES_DIRS = (
 # Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # AWS S3 Configuration for media files only
 if 'USE_AWS' in os.environ:
