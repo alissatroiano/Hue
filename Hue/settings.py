bf227c3e59cabd179bb65afaaa4a05fb1b95b9a2
Alissa Troiano <alissatroianonyc@gmail.com>
2026-01-14 19:09:15 -0500

    Update settings.py

--- Hue/settings.py
+++ Hue/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'shop',
     'cart',
     'checkout',
-       # 3rd party
+    # 3rd party
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django_countries',
     "crispy_bootstrap5",
     "profiles",
+    "storages",
     "hugo"
 ]
 
@@ -170,22 +171,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-USE_AWS = os.environ.get('USE_AWS') == 'True'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = "hue@example.com"
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
-if 'USE_AWS' in os.environ:
+if os.environ.get('USE_AWS') == 'True':
     # Cache Control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
